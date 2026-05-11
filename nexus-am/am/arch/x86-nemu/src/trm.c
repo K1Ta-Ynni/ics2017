94ece55068f786c0c4480fe4081173700addfc8c
tracer-ics2017 <tracer@njuics.org>
2026-05-11 08:23:49 -0700

    >  run 161220000 ynni Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  08:23:49 up  1:00,  1 user,  load average: 0.00, 0.10, 1.84 13389094e82cb877a30ac09939c601cc7e3715ba

--- nexus-am/am/arch/x86-nemu/src/trm.c
+++ nexus-am/am/arch/x86-nemu/src/trm.c
@@ -1,9 +1,6 @@
 #include <am.h>
 #include <x86.h>
 
-// Define this macro after serial has been implemented
-#define HAS_SERIAL
-
 #define SERIAL_PORT 0x3f8
 
 extern char _heap_start;
@@ -16,7 +13,6 @@ _Area _heap = {
 };
 
 static void serial_init() {
-#ifdef HAS_SERIAL
   outb(SERIAL_PORT + 1, 0x00);
   outb(SERIAL_PORT + 3, 0x80);
   outb(SERIAL_PORT + 0, 0x01);
@@ -24,14 +20,11 @@ static void serial_init() {
   outb(SERIAL_PORT + 3, 0x03);
   outb(SERIAL_PORT + 2, 0xC7);
   outb(SERIAL_PORT + 4, 0x0B);
-#endif
 }
 
 void _putc(char ch) {
-#ifdef HAS_SERIAL
   while ((inb(SERIAL_PORT + 5) & 0x20) == 0);
   outb(SERIAL_PORT, ch);
-#endif
 }
 
 void _halt(int code) {
